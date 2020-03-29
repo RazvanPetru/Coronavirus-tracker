f4065a51c876a382f0d7e6d4096e8a540d7ecc93
Razvp <razvanpetru@users.noreply.github.com>
2020-03-29 18:52:21 +0300

    adding countries

--- app.js
+++ app.js
@@ -19,18 +19,16 @@ app.use("/public", express.static("public"));
 
 var baseURL = "https://covid19.mathdro.id/api";
 var countryURL = "https://covid19.mathdro.id/api/countries";
-var countryDetail = `https://covid19.mathdro.id/api/countries/ROMANIA`;
+var countryDetail = `https://covid19.mathdro.id/api/countries/USA`;
 
 app.get("/", (req, res) => {
   axios
     .all([axios.get(baseURL), axios.get(countryURL), axios.get(countryDetail)])
     .then(
       axios.spread((corona, countries, detail) => {
-        console.log(corona.data);
         var corona = corona.data;
         var countries = countries.data.countries.map(item => item.name);
         var detailCountry = detail.data;
-        console.log(detail);
 
         res.render("index", {
           corona: corona,
