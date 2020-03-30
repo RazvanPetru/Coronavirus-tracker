e7bd5a8b53eb6af7dd1ea30d9a5361bf9b74a93f
Razvp <razvanpetru@users.noreply.github.com>
2020-03-30 11:39:17 +0300

    adding countries

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyparser = require("body-parser");
 const request = require("request");
 const ejs = require("ejs");
 const axios = require("axios");
+const moment = require("moment");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,23 +18,39 @@ app.use(
 
 app.use("/public", express.static("public"));
 
-var baseURL = "https://covid19.mathdro.id/api";
-var countryURL = "https://covid19.mathdro.id/api/countries";
-var countryDetail = `https://covid19.mathdro.id/api/countries/USA`;
+var baseURL = "http://api.coronastatistics.live/all";
+var countryURL = "http://api.coronastatistics.live/countries?sort={data.cases}";
+var recoveredURL =
+  "http://api.coronastatistics.live/countries?sort={parameter}";
+var deathsURL = "http://api.coronastatistics.live/countries?sort={cases}";
+var casesURL = "http://api.coronastatistics.live/countries?sort={cases}";
 
 app.get("/", (req, res) => {
   axios
-    .all([axios.get(baseURL), axios.get(countryURL), axios.get(countryDetail)])
+    .all([
+      axios.get(baseURL),
+      axios.get(countryURL),
+      axios.get(recoveredURL),
+      axios.get(deathsURL),
+      axios.get(casesURL)
+    ])
     .then(
-      axios.spread((corona, countries, detail) => {
+      axios.spread((corona, countries, recovered, deaths, cases) => {
         var corona = corona.data;
-        var countries = countries.data.countries.map(item => item.name);
-        var detailCountry = detail.data;
+        var countries = countries.data.map(ctr => ctr.country);
+        var recovered = recovered.data.map(i => i.recovered);
+        var deaths = deaths.data.map(d => d.deaths);
+        var cases = cases.data.map(c => c.cases);
+
+        countries.sort((a, b) => a - b);
 
         res.render("index", {
           corona: corona,
           countries: countries,
-          detailCountry: detailCountry
+          recovered: recovered,
+          deaths: deaths,
+          cases: cases,
+          moment: moment
         });
       })
     )
